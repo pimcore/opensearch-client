15adbe0a091eae1fa481531ebbe0029f1c822af9
Sebastian Blank <sebastian.bl@gmx.de>
2024-04-29 14:48:17 +0200

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -23,11 +23,14 @@ $config->setRules([
     'header_comment'         => [
         'comment_type' => 'PHPDoc',
         'header' => 'Pimcore' . PHP_EOL . PHP_EOL .
-            'This source file is available under following license:' . PHP_EOL .
+            'This source file is available under two different licenses:' . PHP_EOL .
             '- GNU General Public License version 3 (GPLv3)' . PHP_EOL .
+            '- Pimcore Commercial License (PCL)' . PHP_EOL .
+            'Full copyright and license information is available in' . PHP_EOL .
+            'LICENSE.md which is distributed with this source code.' . PHP_EOL .
             PHP_EOL .
             ' @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)' . PHP_EOL .
-            ' @license    http://www.pimcore.org/license     GPLv3'
+            ' @license    http://www.pimcore.org/license     GPLv3 and PCL'
     ],
 
     'blank_line_before_statement'         => true,
@@ -49,6 +52,7 @@ $config->setRules([
     'no_leading_namespace_whitespace'     => true,
     'no_short_bool_cast'                  => true,
     'no_spaces_around_offset'             => true,
+    'no_superfluous_phpdoc_tags'          => ['allow_mixed' => true, 'remove_inheritdoc' => true],
     'no_unneeded_control_parentheses'     => true,
     'no_unused_imports'                   => true,
     'no_whitespace_before_comma_in_array' => true,
@@ -73,3 +77,4 @@ $config->setRules([
 
 $config->setFinder($finder);
 return $config;
+
